d52cb7bf700fab4e3a58d465de3f6ee6154be52e
Omar El Achgar <elachgaromar28@gmail.com>
2025-06-26 23:59:37 +0100

    Add page sign up

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ window.addEventListener("load", function() {
     loader.style.display = 'none' ;
 })
 
+// navigation bar of mobile
 const bar = document.getElementById('bar');
 const navbar = document.getElementById('navbar');
 const close = document.getElementById("close");
@@ -14,3 +15,17 @@ bar.addEventListener('click', () => {
 close.addEventListener("click", () => {
     navbar.classList.remove('activeBar');
 })
+
+
+function togglePassword() {
+    const input = document.getElementById("passwd");
+    const button = event.target; // get l'element cliquer "img"
+
+    if (input.type === "password") {
+        input.type = "text";
+        button.src = "images/open-eye.png";
+    } else {
+        input.type = "password";
+        button.src = "images/eyes.png";
+    }
+}
